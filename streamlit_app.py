5b891acab2a28c543457687e07e7968071d326e7
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-06 15:38:24 +0530

    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvise Fruit Advice')
 try:
   fruit_choice=streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice
+  if not fruit_choice:
     streamlit.error("Please select fruit to get information")
     #streamlit.write('The user entered',fruit_choice)
   else:
