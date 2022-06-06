b7d1544a5bcffc835af92eea91dbcd24ffcbdbae
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-06 12:35:23 +0530

    new section added API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,17 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list= my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+#new section to dispaly fruityvise api response
+streamlit.header('Fruityvise Fruit Advice')
+
+import requests
+fruityvise_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvise_response.json())
